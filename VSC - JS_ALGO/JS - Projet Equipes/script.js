ca394658c743ae491b6f69e1c419130872bfe3c6
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2021-03-08 15:43:12 +0100

    update

---
+++ VSC - JS_ALGO/JS - Projet Equipes/script.js
@@ -0,0 +1,17 @@
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+console.log(button2)
+
+
+
+function clicsurbutton2(){
+    let test = document.querySelector("#test")
+    let input = document.createElement("INPUT");
+    input.setAttribute("type", "text");
+    test.appendChild(input);
+
+
+}
+
+
+button2.addEventListener("click", clicsurbutton2);
