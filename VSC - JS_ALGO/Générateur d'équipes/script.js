c811d2b2c95996a0a4651da8ff222cb8383c10c0
DPatru <73827969+DorianPATRU@users.noreply.github.com>
2021-05-04 11:14:32 +0200

    update

--- VSC - JS_ALGO/Générateur d'équipes/script.js
+++ VSC - JS_ALGO/Générateur d'équipes/script.js
@@ -1,32 +1,80 @@
-const addParticipant = document.querySelector('#addParticipant')
-const randomButton = document.querySelector('#random-button')
-
 const inputList = document.querySelector('#inputList')
 const teams = document.querySelector('#teams')
+const personByTeamInput = document.querySelector('#personByTeam')
 
-let participants = []
+const addParticipant = document.querySelector('#addParticipant')
+addParticipant.addEventListener('click', addParticipantOnClick)
 
-function addNewParticipant() {
+const randomButton = document.querySelector('#random-button')
+randomButton.addEventListener('click', randomButtonOnclick)
 
-  let input = document.createElement('input')
+function addParticipantOnClick() {
+  const input = document.createElement('input')
+  const newParticipantNumber = inputList.childElementCount + 1
   input.setAttribute('type', 'text')
-  input.setAttribute('name', `participant-${inputList.childElementCount + 1}`)
-  input.setAttribute('placeholder', `Participant ${inputList.childElementCount + 1}`)
+  input.setAttribute('name', `participant-${newParticipantNumber}`)
+  input.setAttribute('placeholder', `Participant ${newParticipantNumber}`)
 
   inputList.appendChild(input)
 }
 
-function generateRandomTeam() {
-  participants = []
+function randomButtonOnclick() {
   teams.textContent = '';
 
+  let participants = []
   inputList.querySelectorAll('input')
     .forEach(input => participants.push(input.value))
 
+  const participantsShuffled = shuffleArray(participants)
+  
+  // TODO : 1. Récupérer le nombre de participant par équipe
+  const personByTeam = Number(personByTeamInput.value)
+
+  // TODO : 2. Déterminer le nombre d'équipe
+  const teamsNumber = Math.ceil(participantsShuffled.length / personByTeam) 
+
+  // TODO : 3.a Créer les équipes (uniquement via des tableaux avec la console)
+  // Méthode 1
+  // let teamsShuffled = []
+  // for (let j = 0; j < teamsNumber; j++) { 
+  //   let equipe = []
+  //   for(let i = teamsShuffled.length * personByTeam; i < teamsShuffled.length * personByTeam + personByTeam; i++) {
+  //     if (participantsShuffled[i]) {
+  //       equipe.push(participantsShuffled[i])
+  //     }
+  //   }
+  //   teamsShuffled.push(equipe)
+  // }
+
+  // Méthode 2
+  let teamsShuffled = []
+  let index  = 0
+  while (index < participantsShuffled.length) {
+    teamsShuffled.push(
+      participantsShuffled.slice(index, index + personByTeam)
+    )
+    index = index + personByTeam
+  }
+
+  // for (let index = 0; index < participantsShuffled.length; index += personByTeam) {
+  //   teamsShuffled.push(
+  //     participantsShuffled.slice(index, index + personByTeam)
+  //   )
+  // }
+  
+  // TODO : 3b. Ajouter dans la page les équipes en fonction des tableaux créés dans l'étape a
+  for (let i = 0; i < teamsShuffled.length; i++) {
+    addNewTeamBlock(teamsShuffled[i], i)
+  }
+
+  teamsShuffled.forEach((team, index) => addNewTeamBlock(team, index))
+}
+
+function addNewTeamBlock(participants, i) {
   let team = document.createElement('div')
   team.classList.add('team')
   let title = document.createElement('h1')
-  title.textContent = 'Équipe x'
+  title.textContent = `Équipe ${i+1}`
 
   let list = document.createElement('ul')
 
@@ -39,14 +87,7 @@ function generateRandomTeam() {
   team.appendChild(title)
   team.appendChild(list)
   teams.appendChild(team)
-
-  const participantsShuffled = randomParticipants(participants)
-  console.log(participantsShuffled)
 }
-
-addParticipant.addEventListener('click', addNewParticipant)
-randomButton.addEventListener('click', generateRandomTeam)
-
 
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
@@ -54,33 +95,14 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function randomParticipants(participantsToRandom){
-  const a = [...participantsToRandom]
-  const b = []
+function shuffleArray(arrayToShuffle){
+  const arrayToShuffleCopy = [...arrayToShuffle]
+  const arrayShuffled = []
 
-  // Étape intiale
-  // a['Dorian', 'Ethan', 'Julien', 'Léa', 'Mélanie','Pauline', 'Romain', 'Sandra']
-  // b[]
-
-  // Tant que il y a un élément dans le tableau a = while
-  while(a.length > 0) {
-    const randomIndex = getRandomNumber(0, a.length - 1)
-    b.push(a[randomIndex]) 
-    a.splice(randomIndex, 1)
+  while(arrayToShuffleCopy.length > 0) {
+    const randomIndex = getRandomNumber(0, arrayToShuffleCopy.length - 1)
+    arrayShuffled.push(arrayToShuffleCopy[randomIndex]) 
+    arrayToShuffleCopy.splice(randomIndex, 1)
   }
-  return b
-
-  // Étape 1
-  // Nombre aléatoire entre 0 et 7
-  // a ['Dorian', 'Julien', 'Léa', 'Mélanie','Pauline', 'Romain', 'Sandra']
-  // b ['Ethan']
-
-  // Étape 2
-  // Nombre aléatoire entre 0 et 6
-  // a ['Dorian', 'Julien', 'Léa', 'Mélanie', 'Romain', 'Sandra']
-  // b ['Ethan', 'Pauline']
-
-  // Étape n 
-  // a []
-  // b ['Ethan', 'Pauline', 'Julien', 'Dorian', 'Mélanie', 'Léa', 'Romain', 'Sandra']
+  return arrayShuffled
 }
